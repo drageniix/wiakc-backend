6c3a886d1f5cbe555e96e9734bb5e78116e870a8
Ashley Barrow <drageniix@gmail.com>
2019-01-10 18:08:00 -0600

    8080

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ mongoose
     { useNewUrlParser: true }
   )
   .then(() => {
-    const server = app.listen(process.env.PORT || 5000);
+    const server = app.listen(process.env.PORT || 8080);
     socket.init(server);
   })
   .catch(err => console.log(err));
